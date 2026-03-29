f9c692046a00ed390e9c045d899860cfbbdf7136
Admin <Admin@DESKTOP-Q79K2ON>
2026-03-29 13:07:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    const double PI = 3.14159265358979323846;
+    double sum = 0.0;
+    for (int n = 1; n <= 23; ++n) {
+        double term = pow(2.0, -n) * cos(PI * n / 4.0);
+        sum += term;
+    }
+    std::cout << "a) Sum = " << sum << std::endl;
+    return 0;
+}
